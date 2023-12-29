42ea47f7956eab1f2de1b503b7784219f04472de
M Ali Zahid <aliz0908@hotmail.com>
2023-12-30 02:45:02 +0500

    fixed issue in handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -30,7 +30,7 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        return response()->json($exception->getMessage(), $exception->getCode);
+        return response()->json($exception->getMessage(), $exception->getCode());
     }
 
 }
