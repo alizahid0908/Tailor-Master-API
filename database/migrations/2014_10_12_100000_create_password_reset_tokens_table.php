ab05ae46f7457478f623ccf6cd2a0484c324eaba
M Ali Zahid <aliz0908@hotmail.com>
2024-01-08 17:04:15 +0500

    Live DB connected & added foreign key flag first migration

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+\Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
 
 return new class extends Migration
 {
@@ -12,6 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
+            
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
