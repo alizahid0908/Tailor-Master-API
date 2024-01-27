3e257eea7dc7fcb803e0048ad4d0ff233e1eb888
M Ali Zahid <aliz0908@hotmail.com>
2024-01-27 17:51:33 +0500

    debugging errors in customer delete function

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -100,10 +100,11 @@ class CustomerController extends Controller
     {
         try {
             $customer = Customer::find($id);
-            dd($customer);
+            // dd($customer);
             if (Auth::user()->id !== $customer->user_id) {
                 return response()->json(['message' => 'Unauthorized'], 401);
             }
+            dd(Auth::user()->id);
             
             if (!$customer) {
                 return response()->json(['message' => 'Customer not found'], 404);
